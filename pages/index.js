fac766faf6e4201aeecd8373608bb7215458e88c
silvestrepaul <silvestre_paul@hotmail.com>
2025-04-15 18:55:58 -0700

    15 04 1700

--- pages/index.js
+++ pages/index.js
@@ -27,8 +27,7 @@ export default function Home() {
               <th className="border px-2 py-1">Email</th>
               <th className="border px-2 py-1">Tarjeta</th>
               <th className="border px-2 py-1">Monto</th>
-              <th className="border px-2 py-1">Editar</th>
-              <th className="border px-2 py-1">Eliminar</th>
+              <th className="border px-2 py-1">Acciones</th>
             </tr>
           </thead>
           <tbody>
